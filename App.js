0b088e3c710c41fa24207f5291766c084328a487
Macbook <Kiennmse130280@gmail.com>
2022-10-13 21:37:50 +0700

    First commit

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { LogBox } from "react-native";
+import React, { useState } from "react";
+import AppLoading from "expo-app-loading";
+import useFonts from "./Utils/useFonts";
+import { NavigationContainer } from "@react-navigation/native";
+import { NativeBaseProvider } from "native-base";
+import AppNavigator from "./AppNavigator";
+LogBox.ignoreLogs(["expo-app-loading is deprecated in favor of expo-splash-screen:"]);
 
 export default function App() {
+  const [IsReady, SetIsReady] = useState(false);
+  const LoadFonts = async () => {
+    await useFonts();
+  };
+
+  if (!IsReady) {
+    return (
+      <AppLoading
+        startAsync={LoadFonts}
+        onFinish={() => SetIsReady(true)}
+        onError={() => {}}
+      />
+    );
+  }else
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <StatusBar animated={true} backgroundColor="white" barStyle="default" />
+    <NativeBaseProvider>
+      <AppNavigator />
+    </NativeBaseProvider>
+  </NavigationContainer>
+  // <View style={styles.container}>
+  //   <Text>
+  //     askdjhsakdhsk
+  //   </Text>
+  // </View>
   );
 }
 
