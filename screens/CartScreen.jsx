e5e41dcbabc78c0df41e80f6295ded7625796e4d
Nguyen Manh Kien <kienchothiec1@gmail.com>
2023-04-03 15:06:42 +0700

    fix bug cart screen

--- screens/CartScreen.jsx
+++ screens/CartScreen.jsx
@@ -24,7 +24,7 @@ const CartScreen = () => {
   useEffect(() => {
     if (isFocused) {
       getService();
-      getCartById()(dispatch,username);
+      // getCartById()(dispatch,username);
     }
   }, [isFocused]);
   return (
