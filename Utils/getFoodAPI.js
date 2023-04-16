d44b08bf1181262c303ea40ccd0388a9fbc20b86
Kiennm <kienchothiec1@gmail.com>
2023-04-16 17:25:15 +0700

    update key google api

--- Utils/getFoodAPI.js
+++ Utils/getFoodAPI.js
@@ -2,18 +2,14 @@ import axios from "axios";
 import { BASE_URL } from "../services/baseURL";
 export const fetchData = () => {
     return (dispatch) => {
-      
       axios
         .get(
           BASE_URL +"/foods"
         )
         .then((result) => {
-          let foodActive = result.data.filter((food) => {
-            if (food.status === true) {
-              return food
-            }
-          })
-          dispatch({ type: "GET_ALL_FOOD", payload: foodActive})
+          let foodActive = result.data.filter((food) => food.status === true)
+          let foodActiveTypeFood = foodActive.filter((food) => food.type === false)
+          dispatch({ type: "GET_ALL_FOOD", payload: foodActiveTypeFood})
         });
     };
   };
